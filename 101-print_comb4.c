92dd7602ce41bb4a0921b777b51af239aeba2f3a
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-17 23:36:27 +0200

    0x01 11

---
+++ 101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c, d, e;
+for (c = 0; c <= 9; c++)
+{
+for (d = 0; d <= 9; d++)
+{
+for (e = 0; e <= 9; e++)
+{
+if (c < d && d < e)
+{
+putchar((char)(c + '0'));
+putchar((char)(d + '0'));
+putchar((char)(e + '0'));
+if (c != 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
