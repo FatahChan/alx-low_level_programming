57ee2aa596c26b0db557cbda067ecce0e937361a
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-21 18:02:10 +0200

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_to_98 - prints n to 98
+ * print_to_98 - prints all natural numbers from n to 98
  * @n: starting number
  * Return: void
  */
@@ -10,34 +11,15 @@ if (n < 98)
 {
 for (; n < 98; n++)
 {
-int reverse = 0, remainder;
-
-while (n != 0) {
-remainder = n % 10;
-reverse = reverse * 10 + remainder;
-n /= 10;
-}
-_putchar(reverse + '0');
-_putchar(',');
-_putchar(' ');
+printf("%d, ", n);
 }
 }
 else
 {
 for (; n > 98; n--)
 {
-int reverse = 0, remainder;
-
-while (n != 0) {
-remainder = n % 10;
-reverse = reverse * 10 + remainder;
-n /= 10;
+printf("%d, ", n);
 }
-_putchar(reverse + '0');
-_putchar(',');
-_putchar(' ');
 }
-_putchar('9');
-_putchar('8');
-_putchar('\n');
+printf("98\n");
 }
