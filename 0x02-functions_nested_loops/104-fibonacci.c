9f6306842bc54a5a73bcc67a80cc5d0c1465eae6
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-22 01:37:13 +0200

    104-fibonacci.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,15 +6,15 @@
  */
 int main()
 {
-    unsigned long int a = 1, b = 2, c, i;
-    printf("%lu, %lu, ", a, b);
-    for(i = 2; i < 98; i++)
-    {
-        c = a + b;
-        printf("%lu, ", c);
-        a = b;
-        b = c;
-    }
-    printf("%lu\n", a + b);
-    return 0;
+unsigned long int a = 1, b = 2, c, i;
+printf("%lu, %lu, ", a, b);
+for(i = 2; i <= 98; i++)
+{
+c = a + b;
+printf("%lu, ", c);
+a = b;
+b = c;
+}
+printf("%lu\n", a + b);
+return 0;
 }
