27bc83de23459e6f51c998d5771033b2b46ba654
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-21 18:21:07 +0200

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,8 +4,10 @@
  * @n : the number of times table
  * Return: void
  */
-void print_times_table(int n){
+void print_times_table(int n)
+{
 int i, j, k;
+
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
