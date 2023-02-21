518516bc8b695dfa0942cf5d3aa4ade50d2b96af
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-21 18:26:42 +0200

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,9 +7,7 @@
 void print_times_table(int n)
 {
 int i, j, k;
-
-if (n >= 0 && n <= 15)
-{
+if (n >= 0 && n <= 15) return;
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
@@ -19,9 +17,9 @@ if (k < 10)
 {
 if (j != 0)
 {
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
+    _putchar(' ');
+    _putchar(' ');
+    _putchar(' ');
 }
 _putchar(k + '0');
 }
@@ -40,11 +38,9 @@ _putchar((k / 10) % 10 + '0');
 _putchar(k % 10 + '0');
 }
 if (j != n)
-{
 _putchar(',');
-}
 }
 _putchar('\n');
 }
-}
+
 }
