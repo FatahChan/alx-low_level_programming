ead33aed093f671844d7d2feb79a3f9c30fc1553
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-21 16:31:19 +0200

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,15 @@
 void printsPutChar() {
     printf("_putchar\n");
 }
+/**
+ * putchar - prints _putchar, followed by a new line.
+ *
+ * Return: void
+ */
+void print_alphabet() {
+    for (char i = 'a'; i <= 'z'; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+}
