95ebabb90d5b5a2a60d396f6707cd2da81eaff8f
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-21 18:42:19 +0200

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void){
+int i = 1, j = 2, k, count = 50;
+printf("%d, %d, ", i, j);
+for (k = 0; k < count; k++)
+{
+i = i + j;
+printf("%d, ", i);
+j = i + j;
+printf("%d, ", j);
+}
+}
