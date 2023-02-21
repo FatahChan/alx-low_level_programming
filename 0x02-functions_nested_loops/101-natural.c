9d3f2e20f56a1b2c97c9ca70b601c423445c158b
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-21 18:36:31 +0200

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - check the code
+* Return: Always 0. 
+*/
+int main(void)
+{
+int sum = 0, i, fives = 1, threes = 1;
+while (fives < 1024)
+{
+fives = fives * 5;
+if (fives > 1024)
+{
+break;
+}
+sum = sum + fives;
+}
+while (threes < 1024)
+{
+threes = threes * 3;
+if (threes > 1024)
+{
+break;
+}
+sum = sum + threes;
+}
+printf("%d \n", sum);
+return (0);
+}
