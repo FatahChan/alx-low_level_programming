3c1b9bfb00e10864d31217c74ba73616dab124c5
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-24 01:14:38 +0200

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  print_number(98);
+  _putchar('\n');
+  print_number(402);
+  _putchar('\n');
+  print_number(1024);
+  _putchar('\n');
+  print_number(0);
+  _putchar('\n');
+  print_number(-98);
+  _putchar('\n');
+  return (0);
+}
+void print_number(int n)
+{
+  int i = 1000000000;
+  if (n == 0)
+  {
+    _putchar('0');
+  }
+  if (n < 0)
+  {
+    _putchar('-');
+    n *= -1;
+  }
+  while (i > n)
+  {
+    i /= 10;
+  }
+  while (i > 0)
+  {
+    _putchar((n / i) + '0');
+    n = n % i;
+    i = i / 10;
+  }
+}
+#include <stdio.h>
+#include <stdlib.h>
+
+void _putchar(char c)
+{
+  putchar(c);
+}
