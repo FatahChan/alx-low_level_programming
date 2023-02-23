bcd22830b6d33eaa2dda7f0668b500a5b3fcc016
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-23 15:54:14 +0200

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line of n length
+ * @n: number of times to print \\
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+int i;
+for (i = 0; i < n && n > 0; i++)
+{
+int j;
+for (j = 0; j < i+1; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+}
+_putchar('\n');
+}
