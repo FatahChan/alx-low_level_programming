5c38adb648384e12ba2314bb41ca1d43e2e744df
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-24 01:17:27 +0200

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,24 +6,25 @@
  */
 void print_number(int n)
 {
-  int i = 1000000000;
-  if (n == 0)
-  {
-    _putchar('0');
-  }
-  if (n < 0)
-  {
-    _putchar('-');
-    n *= -1;
-  }
-  while (i > n)
-  {
-    i /= 10;
-  }
-  while (i > 0)
-  {
-    _putchar((n / i) + '0');
-    n = n % i;
-    i = i / 10;
-  }
+int i = 1000000000;
+
+if (n == 0)
+{
+_putchar('0');
+}
+if (n < 0)
+{
+_putchar('-');
+n *= -1;
+}
+while (i > n)
+{
+i /= 10;
+}
+while (i > 0)
+{
+_putchar((n / i) + '0');
+n = n % i;
+i = i / 10;
+}
 }
