a2d91f130c2206031a456f8c8a82067dd296e68e
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-23 17:34:45 +0200

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,30 +5,30 @@
  */
 int main()
 {
-  int i;
-  for (i = 1; i <= 100; i++)
-  {
-    if (i % 3 == 0 && i % 5 == 0)
-    {
-      printf("FizzBuzz");
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
 }
-    else if (i % 3 == 0)
-    {
-      printf("Fizz");
-    }
-    else if (i % 5 == 0)
-    {
-      printf("Buzz");
-    }
-    else
-    {
-      printf("%d", i);
-    }
-    if (i != 100)
-    {
-      printf(" ");
-    }
-  }
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
+}
+}
 printf("\n");
 return (0);
 }
