10f39cf4db6f609df6ad7f9aaf2d17acd477663a
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-23 18:10:15 +0200

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle of size size
+ * @size: size of triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+int i, j;
+for (i = 0; i < size && size > 0; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (j < size - i - 1)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
