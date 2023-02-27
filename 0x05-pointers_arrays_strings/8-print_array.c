c497bf59db1466806043f2b446e7e205a21e87d1
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-27 21:39:54 +0200

    0x05 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointer to an array of integers
+ * @n: number of elements to be printed
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+int i = 0;
+printf("%d", a[i++]);
+while (i < n)
+{
+printf(", %d", a[i++]);
+}
+printf("\n");
+}
