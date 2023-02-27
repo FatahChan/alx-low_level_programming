da32acfa98cd5650c0069a78660795cff26a9580
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-28 01:01:14 +0200

    0x05 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,14 +13,14 @@ int power = 1;
 int result = 0;
 while (s[i] != '\0')
 {
-if (s[i] >= '0' && s[i] <= '9')
+if(numberFound == 0 && s[i] == '-')
 {
-numberFound = 1;
-if (i > 0 && s[i - 1] == '-')
-{
-sign = -1;
+sign *= -1;
 }
-result = result * power + (s[i] - '0');
+if (s[i] >= '0' && s[i] <= '9')
+{numberFound = 1;
+
+result = result * power + (s[i] - '0') * sign;
 power = 10;
 }
 if (numberFound == 1 && (s[i] < '0' || s[i] > '9'))
@@ -29,5 +29,5 @@ break;
 }
 i++;
 }
-return (result * sign);
+return (result);
 }
