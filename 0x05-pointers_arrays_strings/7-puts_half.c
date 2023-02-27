029a56bd61a0c7fcc125480a6164cbfe1e457053
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-27 21:27:17 +0200

    0x05 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: pointer to a string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 == 0)
+{
+i = i / 2;
+}
+else
+{
+i = (i + 1) / 2;
+}
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
