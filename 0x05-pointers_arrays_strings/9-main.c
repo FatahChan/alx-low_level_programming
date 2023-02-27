6bdb5cb2e5f4cf7b29e2538fb72cfdda2a72abb0
Ahmad Fathallah (FatahChan) <ahmadfathallah89@gmail.com>
2023-02-28 00:12:54 +0200

    0x05 9

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -16,3 +16,16 @@ int main(void)
     printf("%s", ptr);
     return (0);
 }
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,6 +13,7 @@ int i = 0;
 while (src[i] != '\0')
 {
 dest[i] = src[i];
+i++;
 }
 dest[i] = '\0';
 return (dest);
