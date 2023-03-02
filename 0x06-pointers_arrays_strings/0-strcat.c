14bb9cabadc046c7957f3d0086cf57a9956d8f3e
ahmad fathallah <ahmadfathallah89@gmail.com>
2023-03-02 09:48:49 +0200

    fixed format 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,20 +5,21 @@
  * @src: pointer to second string
  * Return: returns dest
  */
-char *_strcat(char *dest, char *src){
+char *_strcat(char *dest, char *src)
+{
 int dest_i = 0;
 int src_i = 0;
-while(dest[dest_i] != '\0')
+while (dest[dest_i] != '\0')
 {
 dest_i++;
 }
-while(1)
+while (1)
 {
 dest[dest_i] = src[src_i];
 dest_i++;
 src_i++;
-if(src[src_i] == '\0')
+if (src[src_i] == '\0')
 break;
 }
-return dest;
+return (dest);
 }
