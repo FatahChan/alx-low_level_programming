75764cb5382cf6520550f5e80a65355c3ba432ef
ahmad fathallah <ahmadfathallah89@gmail.com>
2023-03-02 10:09:10 +0200

    fix 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,6 +5,7 @@
  * @n: limit src to n char
  * Return: return dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 int dest_i = 0;
@@ -18,13 +19,8 @@ while (1)
 dest[dest_i] = src[src_i];
 dest_i++;
 src_i++;
-if (src[src_i] == '\0')
+if (src[src_i] == '\0' || src_i == n)
 break;
-if (src_i == n)
-{
-dest[dest_i + 1] = '\0';
-break;
-}
 }
 return (dest);
 }
