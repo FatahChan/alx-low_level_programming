01059c98ffa42741ad3c43856ba4a601058e4dbb
ahmad fathallah <ahmadfathallah89@gmail.com>
2023-03-03 03:15:44 +0200

    modified:   0x06-pointers_arrays_strings/7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,19 +7,20 @@
 char *leet(char *str)
 {
 	int i;
+	int j;
+	char leet[] = "aAeEoOtTlL";
+	char leet2[] = "4433007711";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		for (j = 0; leet[j] != '\0'; j++)
+		{
+			if (str[i] == leet[j])
+			{
+				str[i] = leet2[j];
+				break;
+			}
+		}
 	}
 	return (str);
 }
