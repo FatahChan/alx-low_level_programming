87841ff1a8bd0eeeabab55f3a5b5daafb4463d32
ahmad fathallah <ahmadfathallah89@gmail.com>
2023-03-13 08:27:57 +0200

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,13 +9,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *array = malloc(size * sizeof(char));
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
-	char *array = malloc(size * sizeof(char));
-	unsigned int i;
 
 	for (i = 0; i < size; i++)
 	{
