1dbb70550b1d80052cd42566f0f6551e4fa110b4
ahmad fathallah <ahmadfathallah89@gmail.com>
2023-03-21 01:28:02 +0200

    modified:   0x0E-structures_typedef/dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: struct dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
