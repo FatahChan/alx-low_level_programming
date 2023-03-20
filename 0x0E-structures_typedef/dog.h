90844c9e86fb6b6722874b6db257c3ca8d1c7e1f
ahmad fathallah <ahmadfathallah89@gmail.com>
2023-03-21 01:07:51 +0200

    new file:   0x0E-structures_typedef/README.md 	new file:   0x0E-structures_typedef/dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog information
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: dog information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
